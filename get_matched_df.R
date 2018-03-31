3039b2835ed913196788697b3473c97b391e1834
Adrien Le Guillou <adrien.leguillou@gmail.com>
2018-03-31 15:10:03 +0200

    Create get_matched_df.R

---
+++ get_matched_df.R
@@ -0,0 +1,23 @@
+get_matched_df <- function(matchit_obj, orig_df) {
+  # takes the output of `matchit()` and the original data_frame inputed in to `matchit()`
+  # returns the original data frame with only the matched rows and a new column `pair`
+  
+  match_list <- matchit_obj$match.matrix %>% 
+  as.data.frame() %>% 
+  rownames_to_column() %>%
+  select(rowname, control = 2) %>%
+  filter(!is.na(control)) %>%
+  mutate(pair = as.integer(as.character(rowname)), 
+         rowname = as.integer(as.character(rowname)),
+         control = as.integer(as.character(control)))
+
+match_list <- match_list %>% 
+  select(rowname = control, pair) %>%
+  bind_rows(match_list[c(1, 3)])
+
+matched_df <- orig_df 
+matched_df$rowname <- seq.int(nrow(matched_df))
+
+matched_df <- match_list %>%
+  left_join(matched_df, by = "rowname")
+}
